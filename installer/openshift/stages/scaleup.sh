1ad76618a979a5f83785e0df2700e96f8f41ca89
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2018-10-13 12:46:59 -0300

    Initial support for kubespray installer

---
+++ installer/openshift/stages/scaleup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+case "$TARGET_RELEASE" in
+    v3.9|v3.10)
+        ansible-playbook $@ /usr/share/ansible/openshift-ansible/playbooks/openshift-node/scaleup.yml
+        ;;
+    *)
+        ansible-playbook $@ /usr/share/ansible/openshift-ansible/playbooks/byo/openshift-node/scaleup.yml
+esac
